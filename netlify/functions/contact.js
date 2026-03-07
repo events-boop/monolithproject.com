8ec29d340a562df8851c05931bb97ae918ac9a11
events-boop <events@monolithproject.com>
2026-03-06 19:15:59 -0600

    Revert "chore(backend): integrate Codex serverless security patches and api scripts"
    This reverts commit b70207f8a07a7bc0e7a6c716f866718cbdd47ffd.

--- netlify/functions/contact.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const { handleContactSubmission } = require("./_lib/forms");
-
-exports.handler = async function handler(event) {
-  return handleContactSubmission(event);
-};
--- netlify/functions/join.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const { handleJoinSubmission } = require("./_lib/forms");
-
-exports.handler = async function handler(event) {
-  return handleJoinSubmission(event);
-};
